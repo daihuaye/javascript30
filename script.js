0a929a13e184ef506844af17b82aec50270c0588
Glitch (surf-jail) <none>
2018-01-05 18:38:33 +0000

    📖🎻 04 Flex Panels Image Gallery

--- script.js
+++ script.js
@@ -1,88 +1,16 @@
-// Get your shorts on - this is an array workout!
-// ## Array Cardio Day 1
-// Some data we can work with
-const inventors = [
-  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
-  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
-  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
-  { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
-  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
-  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
-  { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
-  { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
-  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
-  { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
-  { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
-  { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
-];
-const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
-// Array.prototype.filter()
-// 1. Filter the list of inventors for those who were born in the 1500's
-const filtered = inventors.filter(inventor => inventor.year > 1500 && inventor.year < 1600);
-console.log('Filter the list of inventors for those who were born in the 1500\'s');
-console.table(filtered);
+const panels = document.querySelectorAll('.panel');
 
+function toggleOpen() {
+  this.classList.toggle('open');
+}
 
-// Array.prototype.map()
-// 2. Give us an array of the inventors' first and last names
-const names = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
-console.log("Give us an array of the inventors' first and last names");
-console.table(names);
-
-// Array.prototype.sort()
-// 3. Sort the inventors by birthdate, oldest to youngest
-const sorted = inventors.sort((prev, next) => { 
-  return prev.year - next.year > 0 ? 1 : -1;
-});
-console.log("Sort the inventors by birthdate, oldest to youngest");
-console.table(sorted);
-
-// Array.prototype.reduce()
-// 4. How many years did all the inventors live?
-const numYears = inventors.reduce((sum, inventor) => {
-  return sum + (inventor.passed - inventor.year);
-}, 0);
-console.log("How many years did all the inventors live?");
-console.log(numYears);
-
-// 5. Sort the inventors by years lived
-const sortedYears = inventors.sort((prev, next) => {
-  const pYears = prev.passed - prev.year;
-  const nYears = next.passed - next.year;
-  
-  return pYears - nYears > 0 ? -1 : 1;
-});
-console.log("Sort the inventors by years lived");
-console.table(sortedYears);
-
-// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
-// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
-// const cities = Array.from(document.querySelectorAll('.mw-category-group a'));
-// const cityNames = cities.map(city => city.text);
-// const deCities = cityNames.filter(name => name.includes('de'));
-
-// 7. sort Exercise
-// Sort the people alphabetically by last name
-const sortedPeople = people.sort((prev, next) => {
-  const [pLast, pFirst] = prev.split(', ');
-  const [nLast, nFirst] = next.split(', ');
-  
-  return pLast.localeCompare(nLast);
-});
-console.log("Sort the people alphabetically by last name");
-console.table(sortedPeople);
-
-
-// 8. Reduce Exercise
-// Sum up the instances of each of these
-const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
-const result = data.reduce((acc, item) => {
-  if (acc.hasOwnProperty(item)) {
-    acc[item]++;
-    return acc;
+function toggleActive(event) {
+  //this.querySelecor('p:first-child');
+  console.log(event.propertyName);
+  if (event.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
   }
-  acc[item] = 1;
-  return acc;
-}, {});
-console.log("Sum up the instances of each of these");
-console.table(result);
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
