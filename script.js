d7d512d27e44b24c54275c2c7175cabae7060391
Glitch (abrupt-crawdad) <none>
2018-01-30 22:11:22 +0000

    🌊👙 Updated with Glitch for localstroage and eventdelgration

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
-const checkboxes = document.querySelectorAll('input');
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
-let currentCheckbox = undefined;
-
-function onClick(event) {
-  if (event.shiftKey && event.target.checked) {
-    let shouldUpdate = false;
-    currentCheckbox.checked = true;
-    checkboxes.forEach(box => {
-      if (shouldUpdate) {
-        box.checked = true;  
-      }
-      if (box === currentCheckbox || box === event.target) {
-        shouldUpdate = !shouldUpdate;
-      }
-    });
-  }
-  currentCheckbox = event.target;
+function addItem(event) {
+  event.preventDefault();
+  const text = (this.querySelector("[name='item']")).value;
+  const item = {
+    text,
+    done: false
+  };
+  items.push(item);
+  populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));
+  this.reset();
 }
 
-checkboxes.forEach(box => {
-  box.addEventListener('click', onClick)
-});
+function populateList(plates = [], plateList) {
+  plateList.innerHTML = plates.map((plate, i) => {
+    return `
+      <li>
+        <input type="checkbox" data-index="${i}" id="item${i}" ${plate.done ? 'checked' : ''} />
+        <label for="item${i}">${plate.text}</label>
+      </li>
+    `;
+  }).join('');
+}
+
+function toggleDone(event) {
+  if (event.target.matches("[type='checkbox']")) {
+    const index = event.target.dataset.index;
+    items[index].done = !items[index].done;
+    localStorage.setItem('items', JSON.stringify(items));
+  }
+  
+}
+
+addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
+
+populateList(items, itemsList);
